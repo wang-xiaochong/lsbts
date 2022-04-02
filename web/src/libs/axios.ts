ee458eeefe76b7b3fbe97cb87bdfe1d43286d6cb
xiaoshuai <1378334435@qq.com>
2022-04-02 12:22:39 +0800

    axios

--- web/src/libs/axios.ts
+++ web/src/libs/axios.ts
@@ -3,6 +3,10 @@ import Axios from 'axios'
 
 let token = '';
 // let lock = false;
+
+
+
+
 if (typeof (window) != 'undefined') {
     token = localStorage.token || ''
 }
@@ -13,10 +17,54 @@ const axios = Axios.create({
         token: token,
     }
 });
-   
+
+
+
+let pending: any[] = []; //声明一个数组用于存储每个请求的取消函数和axios标识
+let cancelToken = Axios.CancelToken;
+let removePending = (config: any) => {
+    // console.log(config);
+
+    pending.forEach((value, index) => {
+        if (value.url === axios.defaults.baseURL + config.url) { //在当前请求在数组中存在时执行取消函数
+            value.f(); //执行取消操作
+            //pending.splice(index, 1); //根据具体情况决定是否在这里就把pending去掉
+            console.log(config)
+        }
+    })
+}
+
+
+
+
+
+axios.interceptors.request.use(config => {
+    removePending(config); //在一个axios发送前执行一下判定操作，在removePending中执行取消操作
+    // console.log(config.url);
+    config.cancelToken = new cancelToken(function executor(c) {//本次axios请求的配置添加cancelToken
+        pending.push({
+            // url: config.url,
+            url: axios.defaults.baseURL && config.url ? (axios.defaults.baseURL + config.url) : '',
+            f: c
+        });
+        // console.log(axios.defaults.baseURL+config.url);
+        //将本次的url添加到pending中，因此对于某个url第一次发起的请求不会被取消，因为还没有配置取消函数
+    });
+    return Promise.resolve(config);
+}, error => {
+    return Promise.reject(error)
+})
+
+
+
+
 axios.interceptors.response.use(res => {
-    if(res.data === 'Processing'){
-        return { };
+
+    removePending(res.config); //在一个axios响应后再执行一下取消操作，把已经完成的请求从pending中移除
+    // console.log(res.config);//如果返回undefined说明被拦截了
+ 
+    if (res.data === 'Processing') {
+        return {};
     }
     return res
 }, err => {
